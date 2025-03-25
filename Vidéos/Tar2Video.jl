b52e659732a3354a16f1ecd85998b3a602258e60
GaXirs <cedric.amerijckx@gmail.com>
2025-03-25 14:41:27 +0100

    "Petite correction dans un path"

--- Vidéos/Tar2Video.jl
+++ Vidéos/Tar2Video.jl
@@ -2,6 +2,6 @@ using MeshCat
 
 filename = "First_Step"
 TAR_path = joinpath(@__DIR__, "Tar", filename)
-MP4_path = join(@__DIR__, "anim", filename)
+MP4_path = joinpath(@__DIR__, "anim", filename)
 
 MeshCat.convert_frames_to_video(TAR_path  * ".tar", MP4_path  * ".mp4")
