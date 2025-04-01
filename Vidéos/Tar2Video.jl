efd3ce137276fb6257369e6b80810e2a7bd97e03
Cédric Amerijckx <cedric.amerijckx@gmail.com>
2025-04-01 13:49:06 +0200

    Delete Vidéos directory

--- Vidéos/Tar2Video.jl
+++
@@ -1,7 +0,0 @@
-using MeshCat
-
-filename = "First_Step"
-TAR_path = joinpath(@__DIR__, "Tar", filename)
-MP4_path = joinpath(@__DIR__, "anim", filename)
-
-MeshCat.convert_frames_to_video(TAR_path  * ".tar", MP4_path  * ".mp4")
