876e381eac89f11ebd61c0c80479d859c3b842b5
GaXirs <cedric.amerijckx@gmail.com>
2025-04-01 14:09:55 +0200

    gitignore modifications

---
+++ Vidéos/convert_to_video.jl
@@ -0,0 +1,7 @@
+using MeshCat
+
+filename = "First_Walk"
+TAR_path = joinpath(@__DIR__, "Tar", filename)
+MP4_path = joinpath(@__DIR__, "anim", filename)
+
+MeshCat.convert_frames_to_video(TAR_path  * ".tar", MP4_path  * ".mp4")
